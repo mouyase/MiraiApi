fcabb44f1ca088e118979773780ca2949e0be446
wb_guodonghao <wb_guodonghao@meituan.com>
2022-10-13 15:23:01 +0800

    wip: 增加文件遍历

--- pages/api/v1/[...slug].ts
+++ pages/api/v1/[...slug].ts
@@ -1,7 +1,19 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import fs from 'fs'
+
+const path = require('path').resolve('./images1')
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { slug } = req.query
-  res.status(200).json(slug)
+  // res.setHeader('Content-Type', 'text/html; charset=UTF-8')
+  try {
+    const pathStat = fs.statSync(path)
+    if (pathStat?.isDirectory()) {
+
+    }
+    const { slug } = req.query
+    // res.setHeader('Content-Type','image/*')
+    res.status(200).json(pathStat)
+  } catch (e) {
+    res.status(404).end()
+  }
 }
